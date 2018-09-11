c7b96b8b05148f768c86e6145dc627f520a14d15
Bayani Elogada <bayanielogada@gmail.com>
2018-09-11 20:33:08 +0800

    Create airstrike-ng.sh

---
+++ airstrike-ng.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+if [ -z $1 ] do
+  echo "Airstrike-ng v0.8.1 by elogada (c)2018."
+  echo "Usage: airstrike-ng [aircontrol IP address]
+  exit 1
+done
+
+
+sleep 3
+echo "[!] Initiating airstrike-ng..."
+sleep 5
+echo "[ ok ] Shell file is up."
+sleep 1
+echo "[!] Ready dependencies..."
+sleep 1
+echo "[ ok ] Dependencies ready."
+sleep 1
+echo "[+] Airstrike-ng v0.8.1 by elogada (c)2018. All deps not mine."
+echo "[!] Welcome to duty, commander."
+echo "[ ok ] Shell is open."
