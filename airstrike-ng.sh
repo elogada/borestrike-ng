c337ad3d35a20432442f48279d390cb735dc9f2a
root <root@localhost.localdomain>
2018-09-11 22:37:34 +0800

    UI improved.

--- airstrike-ng.sh
+++ airstrike-ng.sh
@@ -1,20 +1,21 @@
 #/bin/bash
-if [ -z $1 ] do
-  echo "Airstrike-ng v0.8.1 by elogada (c)2018."
+sleep 3
+echo -e "\033[1m"[+]"\033[0m"" Airstrike-ng v0.8.1 by elogada. All deps not mine."
+if [ -z $1 ]
+then
   echo "Usage: airstrike-ng [aircontrol IP address]"
   exit 1
-done
+fi
 
-sleep 3
-echo "[!] Initiating airstrike-ng..."
+echo -e "\033[1m"[+]"\033[0m"" Initiating airstrike-ng..."
 sleep 5
-echo "[ ok ] Shell file is up."
+echo -e "\033[32m""\033[1m"[ ok ]"\033[0m"" Shell file is up."
 sleep 1
-echo "[!] Ready dependencies..."
+echo -e "\033[32m""\033[1m"[ ok ]"\033[0m"" Readying dependencies..."
 sleep 1
-echo "[ ok ] Dependencies ready."
+echo -e "\033[32m""\033[1m"[ ok ]"\033[0m"" Dependencies ready."
 sleep 1
-echo "[+] Airstrike-ng v0.8.1 by elogada. All deps not mine."
-echo "[!] Welcome to duty, commander."
-echo "[ ok ] Shell should open by now."
+echo -e "\033[1m"[+]"\033[0m"" Airstrike-ng v0.8.1 by elogada. All deps not mine."
+echo -e "\033[1m"[+]"\033[0m"" Welcome to duty, commander."
+echo -e "\033[32m""\033[1m"[ ok ]"\033[0m" "Shell should open by now."
 nc "$1" 1004
